15fd4258ab7bb11969d6306b5075ce3eb1967ae6
JesusPatate <gg.jesuspatate@orange.fr>
2019-08-29 22:09:48 +0200

    Repository annotation

---
+++ src/main/java/fr/ggautier/arch/annotations/Repository.java
@@ -0,0 +1,7 @@
+package fr.ggautier.arch.annotations;
+
+/**
+ * A Repository mediates between the domain and data mapping layers, acting like an in-memory domain object collection.
+ */
+public @interface Repository {
+}
